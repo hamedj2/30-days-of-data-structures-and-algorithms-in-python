31b8ae52326506a389e505e254a638a32b2eec62
hamed <hamedj2@gmail.com>
2024-01-09 23:54:12 -0600

    Update file names

---
+++ Day 1 - Introduction to Data Structures and Algorithms in Python/Day 1 python file.py
@@ -0,0 +1,37 @@
+print('Day 1 \n')
+
+print('*** Arithmetic Operations ***')
+print(f'Addition -> 5 + 3 = {5 + 3}')
+print(f'Subtraction -> 10 - 2 = {10 - 2}')
+print(f'Multiplication -> 4 * 2 = {4 * 2}')
+print(f'Division -> 16 / 2 = {16 / 2}')
+print(f'Floor Division -> 17 // 2 = {17 // 2}')
+print(f'Modulus -> 18 % 10 = {18 % 10}')
+print(f'Exponentiation -> 2 ** 3 = {2 ** 3}')
+
+print('\n')  # New Line
+
+print('*** Comparison Operations ***')
+print(f'Equal -> 8 == 8 is {8 == 8}')
+print(f'Not Equal -> 7 != 8 is {7 != 8}')
+print(f'Greater Than -> 9 > 8 is {9 > 8}')
+print(f'Less Than -> 7 < 8 is {7 < 8}')
+print(f'Greater Than or Equal To -> 8 >= 8 is {8 >= 8}')
+print(f'Less Than or Equal To -> 8 <= 8 is {8 <= 8}')
+
+print('\n')  # New Line
+
+print('*** Logical Operations ***')
+print(f'and -> True and False is {True and False}')
+print(f'or -> True or False is {True or False}')
+print(f'not -> not True is {not True}')
+
+print('\n')  # New Line
+
+print('*** Bitwise Operations ***')
+print(f'AND -> 5 & 3 = {5 & 3}')
+print(f'OR -> 5 | 3 = {5 | 3}')
+print(f'XOR -> 5 ^ 3 = {5 ^ 3}')
+print(f'NOT -> ~5 = {~5}')
+print(f'Left Shift -> 5 << 1 = {5 << 1}')
+print(f'Right Shift -> 5 >> 1 = {5 >> 1}')
